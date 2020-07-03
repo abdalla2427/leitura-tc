94ca1f7d8f20e6268b730d60dcd10e8e79fca15c
Lucas Abdalla <lucasabdalla@id.uff.br>
2020-07-03 16:11:35 -0300

    merge de csvs funcionando

--- AlgoritmoReconhecimentoDeCarga/Merge Csv de teste com API/merge_csv.py
+++ AlgoritmoReconhecimentoDeCarga/Merge Csv de teste com API/merge_csv.py
@@ -6,7 +6,7 @@ class Constantes:
         self.nomeApi = 'rms01_07_2020____17_38_58_837.csv'
         self.tagInicioDataApi = '____'
 
-        self.nomeTeste = 'registro_de_eventos_07_06_2020.csv'
+        self.nomeTeste = 'registro_de_eventos_01_07_2020.csv'
         self.tagInicioDataTeste = 'registro_de_eventos_'
 
         self.tipoAparelho = "Tipo de aparelho"
@@ -48,9 +48,9 @@ def timestamp_para_hora(timestamp):
 
 def converter_string_para_tempo(hora):
     lista_tempo = hora.split(":")
+    lista_tempo.pop()
     lista_tempo[0] = int(lista_tempo[0]*60*60)
     lista_tempo[1] = int(lista_tempo[1]*60)
-    lista_tempo[2] = int(lista_tempo[2])
     return sum(lista_tempo)
 
 def verificar_se_ocorreu_um_evento(hora_formatada, id_carga, ultimo_valor):
@@ -58,49 +58,53 @@ def verificar_se_ocorreu_um_evento(hora_formatada, id_carga, ultimo_valor):
         tempo_api = converter_string_para_tempo(hora_formatada)
         tempo_teste = converter_string_para_tempo(formatar_hora(hora_dos_eventos_1[0][1]))
 
-        if (tempo_api > tempo_teste):
-            classe_1.append(hora_dos_eventos_1[0][0])
+        if (tempo_api == tempo_teste):
             return hora_dos_eventos_1.pop(0)[0]
-            
-        classe_1.append(ultimo_valor)
-
 
     elif ((id_carga == 2) and (len(hora_dos_eventos_2) >= 1)):
         tempo_api = converter_string_para_tempo(hora_formatada)
         tempo_teste = converter_string_para_tempo(formatar_hora(hora_dos_eventos_2[0][1]))
 
-        if (tempo_api > tempo_teste):
-            classe_2.append(hora_dos_eventos_2[0][0])
+        if (tempo_api == tempo_teste):
             return hora_dos_eventos_2.pop(0)[0]
-        classe_2.append(ultimo_valor)
 
     return ultimo_valor
 
-if (True):#dia_evento_api == dia_evento_teste):
-    ultimo_evento_de_1 = 0
-    ultimo_evento_de_2 = 0
+def main():
+    if (dia_evento_api == dia_evento_teste):
+        ultimo_evento_de_1 = 0
+        ultimo_evento_de_2 = 0
 
-    for index, row in csv_teste.iterrows():
-        if row[constantes.tipoAparelho] == 1:
-            hora_dos_eventos_1.append([row[constantes.tipoEvento], formatar_hora(row[constantes.horaEvento])])
-            ultimo_evento_de_1 = index
-        elif row[constantes.tipoAparelho] == 2:
-            hora_dos_eventos_2.append([row[constantes.tipoEvento], formatar_hora(row[constantes.horaEvento])])
-            ultimo_evento_de_2 = index
-        else:
-            if (ultimo_evento_de_1 > ultimo_evento_de_2):
+        for index, row in csv_teste.iterrows():
+            if row[constantes.tipoAparelho] == 1:
                 hora_dos_eventos_1.append([row[constantes.tipoEvento], formatar_hora(row[constantes.horaEvento])])
                 ultimo_evento_de_1 = index
-            else:
+            elif row[constantes.tipoAparelho] == 2:
                 hora_dos_eventos_2.append([row[constantes.tipoEvento], formatar_hora(row[constantes.horaEvento])])
                 ultimo_evento_de_2 = index
+            else:
+                if (ultimo_evento_de_1 > ultimo_evento_de_2):
+                    hora_dos_eventos_1.append([row[constantes.tipoEvento], formatar_hora(row[constantes.horaEvento])])
+                    ultimo_evento_de_1 = index
+                else:
+                    hora_dos_eventos_2.append([row[constantes.tipoEvento], formatar_hora(row[constantes.horaEvento])])
+                    ultimo_evento_de_2 = index
 
-    ultimo_estado_1 = 0
-    ultimo_estado_2 = 0
+        ultimo_estado_1 = 0
+        ultimo_estado_2 = 0
 
-    for index, row in csv_api.iterrows():
-        hora = timestamp_para_hora(row[constantes.timeStamp])
-        ultimo_estado_1 = verificar_se_ocorreu_um_evento(formatar_hora(hora), 1, ultimo_estado_1)
-        ultimo_estado_2 = verificar_se_ocorreu_um_evento(formatar_hora(hora), 2, ultimo_estado_2)
+        for index, row in csv_api.iterrows():
+            hora = timestamp_para_hora(row[constantes.timeStamp])
+            ultimo_estado_1 = verificar_se_ocorreu_um_evento(formatar_hora(hora), 1, ultimo_estado_1)
+            classe_1.append(ultimo_estado_1)
+            ultimo_estado_2 = verificar_se_ocorreu_um_evento(formatar_hora(hora), 2, ultimo_estado_2)
+            classe_2.append(ultimo_estado_2)
 
-    print(classe_1, classe_2)
+        csv_api["classe_1"] = classe_1
+        csv_api["classe_2"] = classe_2
+
+        csv_api.to_csv('merged.csv')
+
+
+if __name__ == '__main__':
+    main()
